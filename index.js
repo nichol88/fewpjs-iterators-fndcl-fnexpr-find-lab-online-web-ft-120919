c846a64f550b209c765f87008fb3ae6910b80727
Brian Nicholls <nichol88@gmail.com>
2020-04-01 15:57:20 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objArray) {
+  // {year: , result: }
+  let result
+  objArray.find(function(obj) {
+    if (obj.result === 'W') {
+      result = obj.year
+    }
+  })
+  return result
+}
